be12d3647128011e3c3d3d9906d04e77671e64be
unknown <timafra2@mallon.com.br>
2022-11-16 17:39:44 -0300

    Add packages router e vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).use(router).mount('#app')
