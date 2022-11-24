e805a03e0b3b3efb1796491d59678adcf5209b0c
unknown <timafra2@mallon.com.br>
2022-11-24 09:02:22 -0300

    add login files

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,21 +1,38 @@
 <template>
-    <div class="header">
-        Header
-    </div>
+    
+    <nav class="bg-black px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+  <div class="container flex flex-wrap items-center justify-between mx-auto px-10">
+  <a href="#" class="flex items-center">
+      <img src="../assets/mallon-logo.png" class="h-6 mr-3 sm:h-9 rounded-full" alt="Mallon Logo">
+      <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Follow-Up</span>
+  </a>
+  <div class="flex md:order-2">
+        <h5 class="block mr-1 text-sm font-small text-gray-500 dark:text-gray:200">Usuário:</h5>
+        <a href="#" class="block mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">Luccas Drapala</a>
+        <a href="#" class="block text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Sair</a>
+        <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </button>
+  </div>
+  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+    <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Tickets</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Equipe Técnica</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">SCM</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Documentação</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav> 
+
 </template>
 
-<script>
-
-    export default {
-
-    }
-</script>
-
-<style>
-
-    .header{
-        background: black;
-        height: 50px;
-    }
-
-</style>
---
+++ src/views/LoginVue.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <!-- <TheHeaderLogin/> -->
+    <LoginBox/>
+    
+</template>
+
+<script>
+// import TheHeaderLogin from '@/components/TheHeaderLogin.vue';
+import LoginBox from '@/components/LoginBox.vue'
+    export default{
+        components: {
+            // TheHeaderLogin,
+            LoginBox
+        },
+    }
+
+</script>
