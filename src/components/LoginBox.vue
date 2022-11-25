234b9cccc5b075ab85b32b0e3dc6a111f07d81c3
unknown <timafra2@mallon.com.br>
2022-11-25 17:35:26 -0300

    validando alguns erros e colocando spinner no loading

--- src/components/LoginBox.vue
+++ src/components/LoginBox.vue
@@ -6,7 +6,16 @@
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Faça login com a sua conta SCM
                     </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+
+                    <div class="flex p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert" v-if="responseInvalid">
+                            <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                        <span class="sr-only">Info</span>
+                        <div>
+                            <span class="font-medium">Ops!</span> Email e/ou senha inválidos.
+                        </div>
+                    </div>
+
+                    <form class="space-y-4 md:space-y-6" action="#" @submit.stop.prevent="login">
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                             <input 
@@ -35,14 +44,14 @@
                             </div>
                             <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Esqueci minha senha</a>
                         </div>
-                        <button 
+                        <button
+                            :disabled="setSpinner" 
                             type="submit" 
                             class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                            @click.stop.prevent="login"
-                        >  
-                        Login
+                            >
+                            <SpinnerBox v-if="setSpinner"/>  
+                            <span v-else>Login</span>
                         </button>
-                        
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                             Não possui uma conta ? <a href="/register" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Registrar</a>
                         </p>
@@ -56,14 +65,21 @@
 <script>
 import axios from 'axios'
 import Cookie from 'js-cookie'
+import SpinnerBox from '@/components/SpinnerBox.vue'
 
     export default{
-        name: 'Login-user',
+        name: 'login-user',
+
+        components: {
+            SpinnerBox,
+        },
 
         data() {
             return {
                 email: '',
-                password: ''
+                password: '',
+                responseInvalid: false,
+                setSpinner: false
             }
         },
 
@@ -74,14 +90,24 @@ import Cookie from 'js-cookie'
                     password: this.password
                 }
 
+                this.resetResponse()
+                this.setSpinner = true
+
                 axios.post('v1/login', payload).then((response) => {
-                    console.log(response)
                     const token = `${response.data.token_type} ${response.data.access_token}` 
                     Cookie.set('_todoList_token', token, {expires: 30})
-                })
 
+                    this.$store.commit('user/STORE_USER', response.data.data)
+
+                }).catch(() => {
+                    this.responseInvalid = true
+                    this.setSpinner = false
+                })
             },
-             
+            
+            resetResponse () {
+                this.responseInvalid = false
+            }
         },
     }
 
---
+++ src/components/SpinnerBox.vue
@@ -0,0 +1,11 @@
+<template>
+
+    
+        <img
+            src="@/assets/img/spinner.svg"
+            alt=""
+            class="inline-block w-5 h-5"
+        >
+
+
+</template>
