19e3cdc81ee3cf7b003d489ad06a338ae1269414
unknown <timafra2@mallon.com.br>
2022-11-25 11:11:53 -0300

    add dados do usuario no vuex

---
+++ src/store/user/index.js
@@ -0,0 +1,16 @@
+export default{
+    namespaced: true,
+
+    state: () => ({
+        user: {}
+    }),
+
+    mutations: {
+        STORE_USER(state, user) {
+            state.user = user
+        }
+    },
+
+    actions: {}
+
+}
