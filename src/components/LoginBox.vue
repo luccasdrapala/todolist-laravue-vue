db7655500f5e39a08eed661a01cad95e42a33650
unknown <timafra2@mallon.com.br>
2022-11-25 17:49:03 -0300

    validando alguns erros

--- src/components/LoginBox.vue
+++ src/components/LoginBox.vue
@@ -27,7 +27,8 @@
                         <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha</label>
                             <input 
-                                type="password" 
+                                type="password"
+                                required 
                                 placeholder="••••••••" 
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 v-model="password"
@@ -85,6 +86,11 @@ import SpinnerBox from '@/components/SpinnerBox.vue'
 
         methods: {
             login() {
+
+                if(this.email === '' || this.password === ''){
+                    return
+                }
+
                 const payload = {
                     email: this.email,
                     password: this.password
